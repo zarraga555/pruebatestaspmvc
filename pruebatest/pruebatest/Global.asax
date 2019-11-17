e5c9653c26bc342840769cde1f6389472b791cc9
zarraga555 <zarraga555@gmail.com>
2019-11-17 18:48:44 -0400

    Creacion de Proyecto en ASP.NET

---
+++ pruebatest/pruebatest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pruebatest.MvcApplication" Language="C#" %>
---
+++ pruebatest/pruebatest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pruebatest.Startup))]
+namespace pruebatest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
